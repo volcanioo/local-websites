3d7db1e2a3d627f8c71ebb7ff6b3d601db08bb36
volcanioo <arayuzgelistirici@gmail.com>
2020-09-26 17:11:07 +0200

    🎨  data flow and structure updates

---
+++ src/data/data.js
@@ -0,0 +1,9 @@
+import categories from '../data/categories.json';
+import germany from '../data/categories.json';
+import turkey from '../data/categories.json';
+
+export default {
+    categories,
+    germany,
+    turkey
+}
--- src/index.js
+++ src/index.js
@@ -1,2 +1,8 @@
+// Styles
 import './styles/main.scss';
 
+// Data
+import data from './data/data.js';
+
+// App
+const app = document.querySelector('script');
