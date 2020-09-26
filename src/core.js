f1f34d0fe67846204470bfae35b814bd493886a0
volcanioo <arayuzgelistirici@gmail.com>
2020-09-26 18:07:47 +0200

    ✏️  typo issues

--- src/core.js
+++ src/core.js
@@ -1,11 +1,11 @@
 export default {
     /**
-     * Set an element's value by data-model
+     * Set an element's value by data-modal
      * @param modalName
      * @param content
     **/
     setElement(modalName, content) {
-        const elements = document.querySelectorAll('*[data-model='+modalName+']');
+        const elements = document.querySelectorAll('*[data-modal='+modalName+']');
         for (let i = 0; i < elements.length; i++) {
             elements[i].innerHTML = content;
         }
@@ -18,7 +18,7 @@ export default {
      * @param property It must contain in object param
     **/
     setSelect(modalName, object, property) {
-        const selects = document.querySelectorAll('*[data-model='+modalName+']');
+        const selects = document.querySelectorAll('*[data-modal='+modalName+']');
         for (let i = 0; i < selects.length; i++) {
             for (let j = 0; j < Object.keys(object).length; j++) {
                 const option = document.createElement("option");
