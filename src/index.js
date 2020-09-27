761bb0d826aa4e5d1bbcda2f443af5af55fd56e1
volcanioo <arayuzgelistirici@gmail.com>
2020-09-27 13:21:27 +0200

    ✨ list feature developed

--- src/index.js
+++ src/index.js
@@ -9,4 +9,25 @@ import countries from './data/countries.js';
 import dom from './core.js';
 const app = document.querySelector('app');
 dom.setElement('total-country-count', Object.keys(countries).length);
-dom.setSelect('all-cities', countries, 'country_name')
+dom.setSelect('all-cities', countries, 'country_name');
+
+for (const key in categories) {
+    if (categories.hasOwnProperty(key)) {
+        const websites = dom.getWebsitesByCategory(countries['turkey'], countries['germany'], categories[key].id);
+        const categoryTitle = document.createElement('strong');
+        categoryTitle.classList.add('category-title');
+        categoryTitle.innerHTML = categories[key].name;
+        dom.createAnElement('website-list', categoryTitle, 'div');
+        const row = document.createElement('div');
+        row.classList.add('table-view__row');
+        for (let i = 0; i < websites.length; i++) {
+            const column = document.createElement('div');
+            column.classList.add('table-view__column');
+            for (let j = 0; j < Object.keys(websites[i]).length; j++) {
+                column.innerHTML += `<a href="` + websites[i][j].url + `" rel="nofollow" target="_blank">` + websites[i][j].url + `</a>`;
+            }
+            row.appendChild(column);
+        }
+        document.querySelector('[data-modal="website-list"]').appendChild(row);
+    }
+}
